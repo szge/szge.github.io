0947cf351a2c6c6c518faf20a540c20fc82db75a
Jonathan <jchiangconan@gmail.com>
2020-05-27 04:03:14 -0400

    + reload and ammo bar, reload delay, manual reload

--- projects/htmlgame/htmlgame.js
+++ projects/htmlgame/htmlgame.js
@@ -4,11 +4,16 @@ var player;
 var mouseTracker;
 var mousePos = {x: 0, y: 0};
 var mouseDown = false;
-var screenWidth = 800;
+var screenWidth = 900;
 var screenHeight = 500;
 var soundShot = [];
 var soundReload;
 var frameCounter = 0;
+var health = 100;
+var healthbar;
+var ammo = 200;
+var ammobar;
+var reloadDelayFrame = 0;
 
 function startGame(id) {
   player = new playerComponent(30, 30, "blue", 10, 120);
@@ -19,6 +24,8 @@ function startGame(id) {
     soundShot[i] = new sound("shot.mp3");
   }
   soundReload = new sound("reload.mp3");
+  healthbar = new healthbarComponent();
+  ammobar = new ammobarComponent();
 }
 
 var myGameArea = {
@@ -47,7 +54,11 @@ var myGameArea = {
     })
     window.addEventListener('mouseup', function (e) {
       mouseDown = false;
-      soundReload.play();
+      if(ammo == 0 && frameCounter > reloadDelayFrame + 20) {
+        reloadDelayFrame = frameCounter;
+        ammo = 200;
+        soundReload.play();
+      }
     })
   },
   clear : function() {
@@ -77,6 +88,42 @@ function mouseComponent() {
   }
 }
 
+function healthbarComponent() {
+  this.width = player.width;
+  this.height = 5;
+  this.update = function(){
+    this.startX = (player.x + player.width / 2) - this.width / 2;
+    this.startY = (player.y) - this.height - 5;
+
+    context = myGameArea.context;
+    context.fillStyle = "white";
+    context.fillRect(this.startX, this.startY, this.width, this.height);
+
+    context.fillStyle = "red";
+    if(health <= 0) {
+      context.fillRect(this.startX, this.startY, 0, this.height);
+    } else {
+      context.fillRect(this.startX, this.startY, this.width * (health / 100), this.height);
+    }
+  }
+}
+
+function ammobarComponent() {
+  this.width = player.width;
+  this.height = 5;
+  this.update = function(){
+    this.startX = healthbar.startX;
+    this.startY = healthbar.startY - this.height;
+
+    context = myGameArea.context;
+    context.fillStyle = "white";
+    context.fillRect(this.startX, this.startY, this.width, this.height);
+
+    context.fillStyle = "gold";
+    context.fillRect(this.startX, this.startY, this.width * (ammo / 200), this.height);
+  }
+}
+
 function getMousePos(canvas, evt) {
   var rect = myGameArea.canvas.getBoundingClientRect();
   return {
@@ -86,9 +133,10 @@ function getMousePos(canvas, evt) {
 }
 
 function shootBasic() {
-  if(mouseDown) {
-    startX = player.x + player.width / 2;
-    startY = player.y + player.height / 2;
+  if(mouseDown && ammo > 0 && frameCounter > reloadDelayFrame + 144) {
+    ammo--;
+    this.startX = player.x + player.width / 2;
+    this.startY = player.y + player.height / 2;
 
     // add inaccuracy
     inaccuracy = 50;
@@ -98,17 +146,17 @@ function shootBasic() {
     // make sure that the line is always at least the maximum length of the screen
     maxLength = Math.hypot(screenWidth, screenHeight);
 
-    angle = Math.atan2(targetY - startY, targetX - startX);
+    angle = Math.atan2(targetY - this.startY, targetX - this.startX);
 
-    finalX = startX + maxLength * Math.cos(angle);
-    finalY = startY + maxLength * Math.sin(angle);
+    finalX = this.startX + maxLength * Math.cos(angle);
+    finalY = this.startY + maxLength * Math.sin(angle);
 
     // draw path
     context = myGameArea.context;
     context.beginPath();
     context.lineWidth = 5;
     context.lineCap = 'round';
-    context.moveTo(startX, startY);
+    context.moveTo(this.startX, this.startY);
     context.lineTo(finalX, finalY);
     context.stroke();
 
@@ -119,6 +167,14 @@ function shootBasic() {
           soundShot[i].play();
       }
     }
+  }
+}
+
+function reload() {
+  if (frameCounter > reloadDelayFrame + 144) {
+    reloadDelayFrame = frameCounter;
+    ammo = 200;
+    soundReload.play();
   }
 }
 
@@ -143,9 +199,12 @@ function updateGameArea() {
   // always update mouse movement first for responsiveness
   mouseTracker.update();
   shootBasic();
-  if (myGameArea.keys["W".charCodeAt(0)]) {if(player.y > 0) player.y -= 2; }
-  if (myGameArea.keys["A".charCodeAt(0)]) {if(player.x > 0) player.x -= 2; }
-  if (myGameArea.keys["S".charCodeAt(0)]) {if(player.y + player.height < screenHeight) player.y += 2; }
-  if (myGameArea.keys["D".charCodeAt(0)]) {if(player.x + player.width < screenWidth) player.x += 2; }
+  if (myGameArea.keys["W".charCodeAt(0)]) {if(player.y > 0) player.y -= 2;}
+  if (myGameArea.keys["A".charCodeAt(0)]) {if(player.x > 0) player.x -= 2;}
+  if (myGameArea.keys["S".charCodeAt(0)]) {if(player.y + player.height < screenHeight) player.y += 2;}
+  if (myGameArea.keys["D".charCodeAt(0)]) {if(player.x + player.width < screenWidth) player.x += 2;}
+  if (myGameArea.keys["R".charCodeAt(0)]) {reload()}
   player.update();
+  healthbar.update();
+  ammobar.update();
 }
